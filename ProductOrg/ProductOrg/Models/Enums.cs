04814d5ecf46dad1ec55a84afc754565324f6419
Steven Gomez <steven.gomez.c@gmail.com>
2023-05-11 22:05:38 -0500

    New emuns to Activity identification

---
+++ ProductOrg/ProductOrg/Models/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductOrg.Models
+{
+    public enum Activity
+    {
+        None = 0,
+        Working = 1,
+        ShortBreak = 2,
+        LongBreak = 3,
+    }
+}
