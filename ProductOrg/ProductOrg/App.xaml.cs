8b56f0076c81120a5e4b5834661697d0da56005c
Steven Gomez <steven.gomez.c@gmail.com>
2023-05-24 16:13:04 -0500

    Config to use NavigationPage in the app

--- ProductOrg/ProductOrg/App.xaml.cs
+++ ProductOrg/ProductOrg/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ProductOrg
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
