b442624720f726a34743e4e1bc1089b8e935cfb5
Steven Gomez <steven.gomez.c@gmail.com>
2023-06-08 21:55:57 -0500

    Ne SelectableData model used to switch

---
+++ ProductOrg/ProductOrg/Models/SelectableData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductOrg.Models
+{
+    public class SelectableData<T>
+	{
+		public T Data { get; set; }
+		public bool Selected { get; set; }
+	}
+}
