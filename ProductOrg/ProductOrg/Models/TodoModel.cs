baef8a6552e24c3d9ca8e32f9fe7c008f080c58e
Steven Gomez <steven.gomez.c@gmail.com>
2023-06-08 21:56:16 -0500

    New TodoModel

---
+++ ProductOrg/ProductOrg/Models/TodoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductOrg.Models
+{
+    public class TodoModel
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+}
