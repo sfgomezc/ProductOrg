04bc70de9b0e6ab290c85781c11400da5d1e14e8
Steven Gomez <steven.gomez.c@gmail.com>
2023-06-08 22:03:30 -0500

    New ToDoPage to show ToDo list using MVVM pattern

--- ProductOrg/ProductOrg/Views/ToDoPage.xaml.cs
+++ ProductOrg/ProductOrg/Views/ToDoPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using ProductOrg.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +10,7 @@ namespace ProductOrg.Views
         public ToDoPage()
         {
             InitializeComponent();
+            BindingContext = new TodoViewModel(Navigation);
         }
     }
 }
